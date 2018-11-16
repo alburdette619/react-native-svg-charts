6d8e7e5bebcf5872bfbb6fbcb2115803aa6bd2c5
Adam Burdette <alburdet619@gmail.com>
2018-11-16 16:49:48 -0500

    Remove unnecessary trim
    Left this in for testing

--- src/x-axis.js
+++ src/x-axis.js
@@ -61,7 +61,7 @@ class XAxis extends PureComponent {
                     textAnchor="middle"
                     x={ x(value) }
                     dy={ lineIndex ? svg.fontSize * 1.2 : 0 }>
-                    {line.trim()}
+                    {line}
                 </TSpan>
             ))
         }
