c61117db87bdad82991151a4b688bcf8c3151b0a
Adam Burdette <adam@fullstacklabs.co>
2019-03-08 10:14:21 -0500

    Added border radius to the standard bar chart
    Moved the path generation logic to utils and incorporated it into the standard bar chart.

--- src/util.js
+++ src/util.js
@@ -15,6 +15,77 @@ const util = {
         })
     },
 
+    commandsToSvgPath(commands) {
+        return commands
+            .map(command => `${command.marker} ${command.values.join(',')}`)
+            .join(' ')
+            .trim()
+    },
+    coordinatesToPathCommands(
+        x0,
+        y0,
+        x1,
+        y1,
+        borderRadius,
+        showTopBorder,
+        showBottomBorder
+    ) {
+        const commands = []
+        commands.push({ marker: 'M', values: [ x0, y0 ] })
+
+        if (showTopBorder) {
+            const topLeft1 = [ x0 + borderRadius, y0 ]
+            const topLeft2 = [ x0, y0 + borderRadius ]
+            commands.push({ marker: 'L', values: topLeft1 })
+            commands.push({
+                marker: 'C',
+                values: [ ...topLeft1, x0, y0, ...topLeft2 ],
+            })
+            commands.push({ marker: 'L', values: topLeft2 })
+        } else {
+            commands.push({ marker: 'L', values: [ x0, y0 ] })
+        }
+
+        if (showBottomBorder) {
+            const bottomLeft1 = [ x0, y1 - borderRadius ]
+            const bottomLeft2 = [ x0 + borderRadius, y1 ]
+            commands.push({ marker: 'L', values: bottomLeft1 })
+            commands.push({
+                marker: 'C',
+                values: [ ...bottomLeft1, x0, y1, ...bottomLeft2 ],
+            })
+            commands.push({ marker: 'L', values: bottomLeft2 })
+            const bottomRight1 = [ x1 - borderRadius, y1 ]
+            const bottomRight2 = [ x1, y1 - borderRadius ]
+            commands.push({ marker: 'L', values: bottomRight1 })
+            commands.push({
+                marker: 'C',
+                values: [ ...bottomRight1, x1, y1, ...bottomRight2 ],
+            })
+            commands.push({ marker: 'L', values: bottomRight2 })
+        } else {
+            commands.push({ marker: 'L', values: [ x0, y1 ] })
+            commands.push({ marker: 'L', values: [ x1, y1 ] })
+        }
+
+        if (showTopBorder) {
+            const topRight1 = [ x1, y0 + borderRadius ]
+            const topRight2 = [ x1 - borderRadius, y0 ]
+            commands.push({ marker: 'L', values: topRight1 })
+            commands.push({
+                marker: 'C',
+                values: [ ...topRight1, x1, y0, ...topRight2 ],
+            })
+            commands.push({ marker: 'L', values: topRight2 })
+        } else {
+            commands.push({ marker: 'L', values: [ x1, y0 ] })
+        }
+
+        commands.push({ marker: 'Z', values: [] })
+
+        return commands
+    },
+
 }
 
 export const Constants = {
